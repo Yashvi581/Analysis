1e2bcf9c6c4bdbce273d359fdd9707bedea12ccc
Yashvi581 <yashvikansara6@gmail.com>
2025-06-09 13:26:28 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -2,31 +2,36 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px
 
-st.title("CSV Data Explorer")
 
-st.header("Information Page")
+def app():
 
-upload_file = st.file_uploader("Upload File", type='csv')
+  st.title("CSV Data Explorer")
 
-if upload_file is not None:
-  df = pd.read_csv(upload_file)
+  st.header("Information Page")
 
-  st.subheader("Data Preview")
-  st.dataframe(df)
-  st.subheader("Basic Information")
+  upload_file = st.file_uploader("Upload File", type='csv')
 
-
-  st.write(f"Shape: {df.shape}")
-  st.write("Column Names:", df.columns.tolist())
-  st.write("Data Types")
-  st.write(df.dtypes)
+  if upload_file is not None:
+    df = pd.read_csv(upload_file)
+    st.session_state['df'] = df
 
 
-  st.subheader("Missing Values")
-  st.write(df.isnull().sum())
+    st.subheader("Data Preview")
+    st.dataframe(df)
+    st.subheader("Basic Information")
+
+
+    st.write(f"Shape: {df.shape}")
+    st.write("Column Names:", df.columns.tolist())
+    st.write("Data Types")
+    st.write(df.dtypes)
+
+
+    st.subheader("Missing Values")
+    st.write(df.isnull().sum())
   
-  st.subheader("Summary Statistics")
-  st.write(df.describe())
+    st.subheader("Summary Statistics")
+    st.write(df.describe())
 
 
 
